0ec99f8f9c52b1ad7a7eb883c69d78f73889c5a9
leo-peres <leoviep@gmail.com>
2024-08-26 10:11:44 -0300

    Include my solutions to exercises 8-12

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+function isLetterOrNumber(str) {
+    return str.length === 1 && (str.match(/[a-z]/i) || str.match(/[0-9]/i));
+  }
 
+const palindromes = function (str) {
+    let newStr = str.split('').filter(x => isLetterOrNumber(x)).join('').toLowerCase();
+    return newStr === newStr.split('').reverse().join('');
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const comparisson = function(p1, p2) {
+    let yearOfDeath1 = "yearOfDeath" in p1 ? p1.yearOfDeath : 2024;
+    let yearOfDeath2 = "yearOfDeath" in p2 ? p2.yearOfDeath : 2024;
+    return (yearOfDeath1 - p1.yearOfBirth) > (yearOfDeath2 - p2.yearOfBirth);
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((max, x) => max = comparisson(x, max) ? x : max);
+}
+
+const comparissonOld = function(p1, p2) {
+    let yearOfDeath1 = "yearOfDeath" in p1 ? p1.yearOfDeath : 2024;
+    let yearOfDeath2 = "yearOfDeath" in p2 ? p2.yearOfDeath : 2024;
+    return (yearOfDeath2 - p2.yearOfBirth) - (yearOfDeath1 - p1.yearOfBirth);
+}
+
+const findTheOldestOld = function(people) {
+    let new_arr = people.slice().sort(comparisson);
+    return new_arr[0];
 };
 
 // Do not edit below this line
